52011cc41f30b4e6a9542dd5f7b5427cf568767e
akshatsZangula <akshat.sandhaliya@zangula.com>
2024-10-25 02:36:55 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.scss'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='om'>
+      shree ganeshay namha:
+       </div>
+    </>
+  )
+}
+
+export default App
